6f5201709aa0858449c320eab7d7afa662ccd293
Harikrishnan KuppusamyKrishnan <hkkrishn@HarikrihnansMBP.lan>
2024-07-03 16:19:47 -0700

    param annotation

--- index.ts
+++ index.ts
@@ -11,7 +11,12 @@ interface Todo {
 
 axios.get(url).then((res)=>{
   const todo = res.data as Todo;
-  console.log(todo);
   const {id,title,completed} = todo;
-  console.log(`The id is ${id} with ${title} and it is ${completed}`);
+  logTodo(id,title,completed);
 })
+
+//function to log
+//added type annotations in order make order of params
+const logTodo = (id:number,title:string,completed:boolean) =>{
+  console.log(`The id is ${id} with ${title} and it is ${completed}`);console.log(`The id is ${id} with ${title} and it is ${completed}`);
+}
